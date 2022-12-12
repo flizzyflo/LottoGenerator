a0a47669ff813a36b693100f39f9ac5c149e30a6
flizzyflo <vip3r1@gmx.de>
2022-12-12 21:26:05 +0100

    refactoring and splitting functions

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 
 from user_interface.main_window import RandomLottoNumberGenerator
 from random_numbers.random_number_generator import get_random_lotto_numbers
+import tkinter as tk
+
+class test(tk.Tk):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
 
 if __name__ == "__main__":
     root = RandomLottoNumberGenerator(generate_numbers_function= get_random_lotto_numbers)
 
     root.title("Lotto Game")
     root.mainloop()
+
--- user_interface/main_window.py
+++ user_interface/main_window.py
@@ -5,32 +5,34 @@ from settings.settings import ENTRY_SIZE
 
 class RandomLottoNumberGenerator(tk.Tk):
 
-    def __init__(self, 
-                 generate_numbers_function: callable, 
-                 ) -> None:
+    def __init__(self, generate_numbers_function: callable) -> None:
         super().__init__()
         
-        self.generate_button = tk.Button(self,
+        self.generate_button = tk.Button(master= self,
                                          text= "Generate Lotto Numbers",
                                          command= lambda: self.generate_numbers())
 
         self.generate_button.pack(fill= tk.BOTH)
+
+        tk.Button(master= self,
+                  text= "Quit",
+                  command= lambda: quit()).pack(fill= tk.BOTH)
        
         self.super_number_checkbox = ttk.Checkbutton(self, 
                                                     text= "Want Super Number?", 
                                                     command= lambda: self.super_number_required())
         self.super_number_checkbox.pack(anchor=tk.W)
 
-        self.number_input_frame= tk.Frame(master= self)
+        self.number_input_frame = tk.Frame(master= self)
         self.number_input_frame.pack(fill= tk.BOTH)
 
-        self.RANGE_START_NUMBER = 1
-        self.RANGE_END_NUMBER = 49
-        self.NUMBER_AMOUNT = 6
+        self.RANGE_START_REGULAR_NUMBER: int = 1
+        self.RANGE_END_REGULAR_NUMBER: int = 49
+        self.REGULAR_NUMBER_AMOUNT: int = 6
 
-        self.RANGE_SUPER_NUMBER_START = 1
-        self.RANGE_SUPER_NUMBER_END = 9
-        self.SUPER_NUMBER_AMOUNT = 1
+        self.RANGE_SUPER_NUMBER_START: int = 1
+        self.RANGE_SUPER_NUMBER_END: int = 9
+        self.SUPER_NUMBER_AMOUNT: int = 1
 
         tk.Label(master= self.number_input_frame, 
                  text= "Regular Numbers Range Start: ",).grid(row= 0,
@@ -42,29 +44,29 @@ class RandomLottoNumberGenerator(tk.Tk):
                  text= "Regular Numbers Amount: ").grid(row= 0, 
                                                         column= 4)
 
-        self.normal_number_range_start_entry = tk.Entry(master= self.number_input_frame, 
+        self.regular_number_range_start_entry = tk.Entry(master= self.number_input_frame, 
                                                   width= ENTRY_SIZE, 
                                                   justify= tk.CENTER)
-        self.normal_number_range_start_entry.grid(row= 0, 
+        self.regular_number_range_start_entry.grid(row= 0, 
                                             column= 1)
 
-        self.normal_number_range_start_entry.insert(0, self.RANGE_START_NUMBER)
+        self.regular_number_range_start_entry.insert(0, self.RANGE_START_REGULAR_NUMBER)
 
-        self.normal_number_range_end_entry = tk.Entry(master= self.number_input_frame, 
+        self.regular_number_range_end_entry = tk.Entry(master= self.number_input_frame, 
                                                 width= ENTRY_SIZE, 
                                                 justify= tk.CENTER)
-        self.normal_number_range_end_entry.grid(row= 0, 
+        self.regular_number_range_end_entry.grid(row= 0, 
                                           column= 3)
 
-        self.normal_number_range_end_entry.insert(0, self.RANGE_END_NUMBER)
+        self.regular_number_range_end_entry.insert(0, self.RANGE_END_REGULAR_NUMBER)
 
-        self.normal_number_amount_entry = tk.Entry(master= self.number_input_frame, 
+        self.regular_number_amount_entry = tk.Entry(master= self.number_input_frame, 
                                              width= ENTRY_SIZE, 
                                              justify= tk.CENTER)
-        self.normal_number_amount_entry.grid(row= 0, 
+        self.regular_number_amount_entry.grid(row= 0, 
                                        column= 5)
 
-        self.normal_number_amount_entry.insert(0, self.NUMBER_AMOUNT)
+        self.regular_number_amount_entry.insert(0, self.REGULAR_NUMBER_AMOUNT)
 
         tk.Label(master= self.number_input_frame, 
                  text= "Super Number Range Start: ").grid(row= 1, 
@@ -102,9 +104,9 @@ class RandomLottoNumberGenerator(tk.Tk):
         self.result_frame = tk.Frame(master= self)
         self.result_frame.pack(fill= tk.BOTH)
 
-        self.random_regular_numbers_label = tk.Label(master= self.result_frame, 
+        self.regular_numbers_label = tk.Label(master= self.result_frame, 
                                                      justify= tk.LEFT)
-        self.random_regular_numbers_label.pack(fill=tk.BOTH, 
+        self.regular_numbers_label.pack(fill=tk.BOTH, 
                                                anchor=tk.W)
 
         self.super_number_label = tk.Label(master= self.result_frame, 
@@ -112,11 +114,14 @@ class RandomLottoNumberGenerator(tk.Tk):
         self.super_number_label.pack(fill= tk.BOTH, 
                                      anchor=tk.W)
 
-        self.generate_numbers_function = generate_numbers_function
-        self.super_number_is_required = False
+        self.generate_numbers_function: callable = generate_numbers_function
+        self.super_number_is_required: bool = False
 
 
     def super_number_checkbox_selected(self) -> bool:
+
+        """Checks the selection state of the super number checkbox. Returns true if selected, otherwise false"""
+
         return "selected" in self.super_number_checkbox.state()
 
 
@@ -126,28 +131,50 @@ class RandomLottoNumberGenerator(tk.Tk):
         
         if self.super_number_checkbox_selected():
             self.super_number_is_required = True
-            self.manage_super_number_state(tk.NORMAL)
+            self.manage_super_number_entry_widget_state(tk.NORMAL)
         
         else:
             self.super_number_is_required = False
-            self.manage_super_number_state(tk.DISABLED)
+            self.manage_super_number_entry_widget_state(tk.DISABLED)
 
 
-    def manage_super_number_state(self, new_state: Literal) -> None:
+    def manage_super_number_entry_widget_state(self, new_state: Literal) -> None:
+        
+        """Manages the entry widgets for the super numbers depending on whether the super number checkbox is selected or not"""
+        
         self.super_number_range_start_entry.config(state= new_state)
         self.super_number_range_end_entry.config(state= new_state)
         self.super_number_amount_entry.config(state= new_state)
 
 
     def get_number_ranges(self) -> None:
-        self.RANGE_START_NUMBER = int(self.normal_number_range_start_entry.get())
-        self.RANGE_END_NUMBER = int(self.normal_number_range_end_entry.get())
-        self.NUMBER_AMOUNT = int(self.normal_number_amount_entry.get())
+        
+        """Wrapper function to grab the numbers from the respective entry widgets and store them within instance variables."""
+        
+        self.get_regular_numbers()
+        self.get_super_numbers()
+
+    
+    def get_regular_numbers(self) -> None:
+
+        """Gets the regular numbers from the respective entry widgets."""
+
+        self.RANGE_START_REGULAR_NUMBER = int(self.regular_number_range_start_entry.get())
+        self.RANGE_END_REGULAR_NUMBER = int(self.regular_number_range_end_entry.get())
+        self.REGULAR_NUMBER_AMOUNT = int(self.regular_number_amount_entry.get())
+
+
+    def get_super_numbers(self) -> None:
+        
+        """Gets the super numbers from the respective entry widgets. Beforehand, it checks whether supernumbers are active or not."""
 
         if self.super_number_checkbox_selected():
             self.RANGE_SUPER_NUMBER_START = int(self.super_number_range_start_entry.get())
             self.RANGE_SUPER_NUMBER_END = int(self.super_number_range_end_entry.get())
             self.SUPER_NUMBER_AMOUNT = int(self.super_number_amount_entry.get())
+
+        else:
+            pass
 
 
     def generate_numbers(self) -> None:
@@ -155,28 +182,44 @@ class RandomLottoNumberGenerator(tk.Tk):
         """Generates the random numbers either with or without supernumber. Updates the labels"""
 
         self.get_number_ranges()
-        self.super_number_label.config(text= "Super Number: -")
+        self.generate_super_numbers()
+        self.generate_regular_numbers()
+
+
+    def generate_super_numbers(self) -> None:
+
+        """Generates the super numbers and updates the label"""
 
         if self.super_number_is_required:
             super_numbers = self.generate_numbers_function(range_start_number= self.RANGE_SUPER_NUMBER_START, 
-                                                               range_end_number= self.RANGE_SUPER_NUMBER_END,
-                                                               required_amount_of_numbers= self.SUPER_NUMBER_AMOUNT)
+                                                           range_end_number= self.RANGE_SUPER_NUMBER_END,
+                                                           required_amount_of_numbers= self.SUPER_NUMBER_AMOUNT)
 
             self.update_label(numbers= super_numbers, 
                               string= "Super Number", 
                               number_label= self.super_number_label)
 
+        else:
+            self.super_number_label.config(text= "Super Number: -")
 
-        regular_numbers = self.generate_numbers_function(range_start_number= self.RANGE_START_NUMBER, 
-                                                                range_end_number= self.RANGE_END_NUMBER, 
-                                                                required_amount_of_numbers= self.NUMBER_AMOUNT)
+
+
+    def generate_regular_numbers(self) -> None:
+ 
+        """Generates the regular numbers and updates the label"""
+
+        regular_numbers = self.generate_numbers_function(range_start_number= self.RANGE_START_REGULAR_NUMBER, 
+                                                         range_end_number= self.RANGE_END_REGULAR_NUMBER, 
+                                                         required_amount_of_numbers= self.REGULAR_NUMBER_AMOUNT)
         
         self.update_label(numbers= regular_numbers,
                           string= "Regular Numbers", 
-                          number_label= self.random_regular_numbers_label)
+                          number_label= self.regular_numbers_label)
 
 
     def update_label(self, numbers: int | list[int], string: str, number_label: tk.Label) -> None:
+
+        """Updates the label according to the numbers generated."""
 
         if numbers == 0:
             number_label.config(text= f"'Range Max - Range Min' has to be bigger as or equal to the amount of numbers to  generate.")
